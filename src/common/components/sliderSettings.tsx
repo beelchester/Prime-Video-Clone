c0cee6c25cba0d91574faf2ff6827dc103c7882e
sahilyeole <sahilyeole93@gmail.com>
2022-11-07 23:14:17 +0530

    fixed arrow

--- src/common/components/sliderSettings.tsx
+++ src/common/components/sliderSettings.tsx
@@ -2,7 +2,7 @@ function SampleNextArrow(props:any) {
       const { className, style, onClick } = props;
       return (
             <div
-          className={`${className} hover:bg-[#0000006b] h-[11rem] mt-1 w-[4rem] absolute right-[19px] z-20 pt-[4.8rem] pl-[1.3rem]`}
+          className={`${className} hover:bg-[#0000006b] h-[11rem] mt-1 w-[4rem] absolute right-[0px] z-20 pt-[4.8rem] pl-[1.3rem]`}
           style={{ ...style}}
           onClick={onClick}
           
