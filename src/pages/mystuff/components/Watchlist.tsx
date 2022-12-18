de81394e87dd2bcf442f65c4a10215bb25a985d9
Sahil Yeole <sahilyeole93@gmail.com>
2022-12-18 17:24:42 +0530

    responsive watchlist

--- src/pages/mystuff/components/Watchlist.tsx
+++ src/pages/mystuff/components/Watchlist.tsx
@@ -16,7 +16,7 @@ const Watchlist: React.FC<props> = ({list}) => {
     <div >
       <div className="font-sans  mt-[1.5rem] ml-[3rem] flex justify-start items-center  relative">
         <h1 className="ml-[0.6rem] font-semibold text-[19px] absolute top-0">Watchlist</h1>
-  <div className=" max-w-full grid  grid-cols-4 gap-[10px] mt-[40px]" >
+  <div className=" max-w-full grid  grid-cols-4 gap-[10px] 2xl:grid-cols-5 mt-[40px]" >
     {list?.map( movie  => movie?.backdrop_path!==null&&  (
     <div>
     <WatchlistCard key={movie.id} movie={movie}/>
